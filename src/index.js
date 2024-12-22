b5241ed129366d5130edefaacbdbd1cbefbeded2
”EvanO3” <evan.oni@hotmail.com>
2024-12-21 21:06:34 -0500

    connected to mongo DB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 require("dotenv").config();
 const routes = require("./Routes/Users.js")
+const mongoose = require("mongoose")
 const app = express();
+//this string will connect to the mongoDB, if error occurs it logs it
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>console.log("Connected to DB"))
+.catch((err)=> console.log(`Error: ${err}`))
 //for production change the OR
 const PORT = process.env.PORT || 4000
 
