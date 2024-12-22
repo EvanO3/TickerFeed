8c69a06a76507b535c210181ca566ef7cabe65c3
”EvanO3” <evan.oni@hotmail.com>
2024-12-22 12:18:54 -0500

    connected to mongoDB & added user creation route

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ require("dotenv").config();
 const routes = require("./Routes/Users.js")
 const mongoose = require("mongoose")
 const app = express();
+
+//allowing to parse json bodies
+app.use(express.json())
 //this string will connect to the mongoDB, if error occurs it logs it
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=>console.log("Connected to DB"))
-.catch((err)=> console.log(`Error: ${err}`))
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => {console.error(`Error: ${err}`);
+  //process.exit will exit the connect if it fails to connect to the DB
+    process.exit(1);
+});
+
+
+
+
 //for production change the OR
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || ""
 
 app.use(routes)
 app.listen(PORT, ()=>{
