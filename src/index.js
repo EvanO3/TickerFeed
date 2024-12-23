b2f7a7d4673f69e1d850499ff0c24fd14dfde8e4
”EvanO3” <evan.oni@hotmail.com>
2024-12-22 22:34:33 -0500

    added cookie parse and changed db logic

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 const express = require('express');
 require("dotenv").config();
-const routes = require("./Routes/Users.js")
-const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
+const routes = require("./Routes/Routes")
+const connectDB = require("./db/db.js")
 const app = express();
 
-//allowing to parse json bodies
+// middleware to allow parsing json bodies
 app.use(express.json())
-//this string will connect to the mongoDB, if error occurs it logs it
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => {console.error(`Error: ${err}`);
-  //process.exit will exit the connect if it fails to connect to the DB
-    process.exit(1);
-});
+//for cookies
 
+app.use(cookieParser());
+//this string will connect to the mongoDB, if error occurs it logs it
+connectDB();
 
 
 
