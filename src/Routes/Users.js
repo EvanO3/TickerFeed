cc20349581c28eef5fd61f4e6e12dc97e0dc4ffe
”EvanO3” <evan.oni@hotmail.com>
2024-12-22 22:35:05 -0500

    added login and logout routes

--- src/Routes/Users.js
+++ src/Routes/Users.js
@@ -3,7 +3,14 @@ const router = Router();
 const userController = require("../controller/userController.js")
 const { checkSchema, validationResult } = require("express-validator");
 const userValidator = require("../utils/userValidation.js")
+const loginValidation = require("../utils/loginValidation.js")
 const handleValidation = require("../utils/handleValidationError.js")
-router.post("/api/register", checkSchema(userValidator.userValidation),handleValidation.handleValidationError,userController.createUser,);
+const auth = require("../utils/Auth.js")
 
+router.post("/register", checkSchema(userValidator.userValidation),handleValidation.handleValidationError,userController.createUser,);
+router.post("/login", checkSchema(loginValidation.loginValidation), handleValidation.handleValidationError,userController.login)
+router.get("/logout", userController.logout)
+router.get("/homePage", auth.authenticate, (req,res)=>{
+    res.send({msg:`Welcome ${req.user.name}`})
+})
 module.exports=router;
