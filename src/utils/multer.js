08950d4f689729f49313d3ce14639cf2312d9ee0
”EvanO3” <evan.oni@hotmail.com>
2024-12-24 13:44:56 -0500

    added validations to only allow PDF files to be used

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -19,8 +19,27 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage:storage,
   //adding validaions for file size etc 1MB file upload
- limits: { fileSize: 10*1024*1024}
+ limits: { fileSize: 10*1024*1024},
   //file type validation to be added after we find out what resume file type we will accept i.e pdf,.docs 
+  fileFilter: function(req, file, cb){
+    checkFileType(file,cb)
+  }
 });
 
+
+//fileType validation to ensure we only get PDF's
+function checkFileType(file, cb){
+    const fileTypes = /pdf/;
+    //matching the extension
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = fileTypes.test(file.mimetype)
+
+    if(mimetype && extname){
+        return cb(null, true)
+    }
+    else{
+        cb('Errpr: PDF files only')
+    }
+    
+}
 module.exports=upload
