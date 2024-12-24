b24a5e82b68b78dfee510fa0adbb8899d0d2accf
”EvanO3” <evan.oni@hotmail.com>
2024-12-24 01:29:52 -0500

    added the routing for uplaoding files

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,8 +1,8 @@
 const Router = require("express")
 const UserRoutes = require("../Routes/Users.js")
-
+const UploadRoutes = require("../Routes/FileUpload.js")
 const router =Router()
 
 router.use("/api",UserRoutes)
-
+router.use("/api", UploadRoutes);
 module.exports=router;
