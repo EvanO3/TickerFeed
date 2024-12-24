3bb492025e003069c8a97a7a9ca0299cb6364b8b
”EvanO3” <evan.oni@hotmail.com>
2024-12-24 01:32:29 -0500

    moved cookie parser middleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser")
 const routes = require("./Routes/Routes")
 const connectDB = require("./db/db.js")
+
+
 const app = express();
 
 // middleware to allow parsing json bodies
 app.use(express.json())
+app.use(cookieParser());
+app.use(routes)
 //for cookies
 
-app.use(cookieParser());
 //this string will connect to the mongoDB, if error occurs it logs it
 connectDB();
 
@@ -18,7 +21,7 @@ connectDB();
 //for production change the OR
 const PORT = process.env.PORT || ""
 
-app.use(routes)
+
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
 })
