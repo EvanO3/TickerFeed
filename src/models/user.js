2a02fa939e8282629ee461a69570588f219bc006
”EvanO3” <evan.oni@hotmail.com>
2024-12-30 18:56:41 -0500

    changed user model to better represent experience, education fields

--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,46 @@
 const {Schema} = require("mongoose")
 const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, maxLength: 25 },
-  email:{type:String, required:true, unique:true},
-  //password length will be validated in untils
-  password: { type: String, required: true},
-  resumes:[{
-    fileUri:String,
-    uploadedAt:{type:Date, default:Date.now()}
-  }],
-  skills:[String],
-  experience:[String],
-  date_of_birth: {type: Date}
-  //add roles if payment is involved
-  
-},
-{timestamps:true}
+const userSchema = new mongoose.Schema(
+  {
+    firstname: { type: String, required: true, maxLength: 25 },
+    lastname: { type: String, required: true, maxLength: 25 },
+    email: { type: String, required: true, unique: true },
+    //password length will be validated in untils
+    password: { type: String, required: true },
+    resumes: [
+      {
+        fileUri: String,
+        uploadedAt: { type: Date, default: Date.now() },
+      },
+    ],
+    education: [
+    {
+        description:String,
+        end_date:String,
+        institute:String,
+        location:String,
+        start_date:String,
+        title:String
+     },
+],
+    skills: [String],
+    experience: [
+      {
+        company: String,
+        description: String,
+        end_date: String,
+        location: String,
+        start_date: String,
+        title: String,
+      },
+    ],
+    Languages: [String],
+    certificates: [String],
+    date_of_birth: { type: Date },
+    //add roles if payment is involved
+  },
+  { timestamps: true }
 );
 
 
