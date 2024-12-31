9e47082f92b7116c78a2c8ce1b8faf5cbd47b2e4
”EvanO3” <evan.oni@hotmail.com>
2024-12-31 13:08:15 -0500

    removed manual auth and added clerk auth

--- src/Routes/Users.js
+++ src/Routes/Users.js
@@ -6,11 +6,14 @@ const userValidator = require("../utils/userValidation.js")
 const loginValidation = require("../utils/loginValidation.js")
 const handleValidation = require("../utils/handleValidationError.js")
 const auth = require("../utils/Auth.js")
+const {clerkClient, requireAuth} = require("@clerk/express")
 
-router.post("/register", checkSchema(userValidator.userValidation),handleValidation.handleValidationError,userController.createUser,);
+router.post("/register", checkSchema(userValidator.userValidation),handleValidation.handleValidationError,userController.createUser);
 router.post("/login", checkSchema(loginValidation.loginValidation), handleValidation.handleValidationError,userController.login)
 router.get("/logout", userController.logout)
-router.get("/homePage", auth.authenticate, (req,res)=>{
-    res.send({msg:`Welcome ${req.user.name}`})
-})
+
+//Clerk expects a front-end route for signin so change when front-end is made
+router.get("/homePage", requireAuth({ signInUrl:"/login"}), (req, res) => {
+  res.send({ msg: `Welcome ${req.user.name}` });
+});
 module.exports=router;
