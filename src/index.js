187e9ab5db960339a057eeb3726675789e5dd239
”EvanO3” <evan.oni@hotmail.com>
2024-12-31 13:07:12 -0500

    added clerk dependency

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 require("dotenv").config();
-const cookieParser = require("cookie-parser")
-const routes = require("./Routes/Routes")
-const connectDB = require("./db/db.js")
-
+const cookieParser = require("cookie-parser");
+const routes = require("./Routes/Routes");
+const connectDB = require("./db/db.js");
+const { clerkMiddleware, requireAuth } = require("@clerk/express");
 
 const app = express();
-
+//offers authentication for application
+app.use(clerkMiddleware())
 // middleware to allow parsing json bodies
 app.use(express.json())
 app.use(cookieParser());
