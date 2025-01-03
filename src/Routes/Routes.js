974141408e1392e1a7e9c2b8e21b98a00a92cfd3
”EvanO3” <evan.oni@hotmail.com>
2025-01-02 23:21:59 -0500

    added a router to control backend api's

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,7 @@ const UserRoutes = require("../Routes/Users.js")
 const UploadRoutes = require("../Routes/FileUpload.js")
 const router =Router()
 
+
 router.use("/api",UserRoutes)
 router.use("/api", UploadRoutes);
 module.exports=router;
