90bc2251916385bf1ffa4c696a3f01b7c49458bb
”EvanO3” <evan.oni@hotmail.com>
2025-01-02 23:22:41 -0500

    added passport authentication to login route

--- src/Routes/Users.js
+++ src/Routes/Users.js
@@ -6,10 +6,16 @@ const userValidator = require("../utils/userValidation.js")
 const loginValidation = require("../utils/loginValidation.js")
 const handleValidation = require("../utils/handleValidationError.js")
 const auth = require("../utils/Auth.js")
-const {clerkClient, requireAuth} = require("@clerk/express")
-
+const passport = require("passport")
+const {localStategy} = require("../utils/local-strategy.js")
 router.post("/register", checkSchema(userValidator.userValidation),handleValidation.handleValidationError,userController.createUser);
-router.post("/login", checkSchema(loginValidation.loginValidation), handleValidation.handleValidationError,userController.login)
+router.post(
+  "/login",
+  checkSchema(loginValidation.loginValidation),
+  handleValidation.handleValidationError,
+  passport.authenticate('local', { session: false }),
+  userController.login
+);
 router.get("/logout", userController.logout)
 
 //Clerk expects a front-end route for signin so change when front-end is made
