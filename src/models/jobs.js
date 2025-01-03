64e3616092fc88e78ce39a670814834740a09ad4
”EvanO3” <evan.oni@hotmail.com>
2025-01-02 23:23:08 -0500

    added job model for saving jobs to db

--- src/models/jobs.js
+++ src/models/jobs.js
@@ -1,1 +1,15 @@
-//job schema, which will take the search criteria of what the user wants 
+const { default: mongoose } = require("mongoose");
+
+//job schema, which will take the search criteria of what the user wants const mongoose = require("mongoose")
+const jobSchema = new mongoose.Schema({
+  jobTitle: { type: String, required: true },
+  company: { type: String, required: true },
+  location: { type: String, required: true },
+  jobDescription: { type: String, required: true },
+  responsibilities: [String],
+  requiredQualifications: [String],
+  datePosted: { type: Date, default: Date.now },
+});
+  jobSchema.index({ jobTitle: 1, company: 1, location: 1 });
+const Job = mongoose.model("jobSchema", jobSchema)
+module.exports= Job
