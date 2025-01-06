2a0a1aa710f8662b7081a11efdd32fe47366b9cf
”EvanO3” <evan.oni@hotmail.com>
2025-01-05 21:51:24 -0500

    deleted jobs route from router

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,11 @@
 const Router = require("express")
 const UserRoutes = require("../Routes/Users.js")
 const UploadRoutes = require("../Routes/FileUpload.js")
-const jobRouter = require("../Routes/Jobs.js")
+
 const router =Router()
 
 
 router.use("/api",UserRoutes)
 router.use("/api", UploadRoutes);
-router.use("/api", jobRouter);
+
 module.exports=router;
