c8a83bf878bfe20b71ba76eb7fa2e8caadede9db
”EvanO3” <evan.oni@hotmail.com>
2025-01-07 23:32:14 -0500

    added controller to search and get information for specific stocks

--- src/controller/stockController.js
+++ src/controller/stockController.js
@@ -11,14 +11,54 @@ const getStockInfo = async (req,res)=>{
     }
     //const stockData = await Stocks.findOne({tickerSymbol:stockSymbol})
    
-      const response = await axios.get(
+      const stockResponse = await axios.get(
         `https://api.polygon.io/v3/reference/tickers/${stockSymbol}`,
         {
           params: {
             apikey: process.env.polygon_key,
           },
         }
-      );
+
+    )
+
+    const newsResponse = await axios.get(
+        `https://api.polygon.io/v2/reference/news?ticker=${stockSymbol}&limit=5`,
+        {
+            params:{
+                apikey:process.env.polygon_key
+            }
+        }
+        
+
+    );
+
+    console.log(newsResponse.data)
+    const StockData={
+        tickerSymbol:stockSymbol,
+        companyName:stockResponse.data.results.name,
+        sector:'',
+        industry:'',
+        marketCap:stockResponse.data.results.market_cap,
+        price:0, // price will be set using vantage api
+        newsArticles:newsResponse.data.results.map(article=>({
+            title:article.title,
+            description:article.description,
+            author:article.author,
+            url:article.article_url,
+            publishedAt:new Date(article.published_utc)
+            
+        }))
+    }
+
+    const stock = await Stocks.findOneAndUpdate(
+        {tickerSymbol:stockSymbol},
+        StockData,
+        {new:true,upsert:true}
+
+    );
+
+    return res.status(200).json({data:stock})
+      
   
      
     
