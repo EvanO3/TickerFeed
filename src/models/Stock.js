6bcf7615a43ff592cfca7981389a4747da0330c8
”EvanO3” <evan.oni@hotmail.com>
2025-01-07 23:32:57 -0500

    added author and desc properties to news article in stock model

--- src/models/Stock.js
+++ src/models/Stock.js
@@ -11,8 +11,10 @@ const stockSchema = new mongoose.Schema({
   newsArticles: [
     {
       title: { type: String, trim: true },
+      description: { type: String, trim: true },
+      author: { type: String, trim: true },
       url: { type: String, match: /^https?:\/\// },
-      publishedAt: {type:Date, required:true },
+      publishedAt: { type: Date, required: true },
     },
   ],
   lastUpdated: { type: Date, default: Date.now },
