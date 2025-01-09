94dba02508959a304b95da00db6f6bb976baf275
”EvanO3” <evan.oni@hotmail.com>
2025-01-08 21:29:38 -0500

    added limiting to all other endpoints

--- src/index.js
+++ src/index.js
@@ -3,14 +3,42 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const routes = require("./Routes/Routes");
 const connectDB = require("./db/db.js");
-
-
+const rateLimit = require('express-rate-limit')
+const slowDown =require("express-slow-down")
 const app = express();
-//offers authentication for application
 
+//Before moving to prod, change the limtor here to 20 and then the more expensive ones like external api calls
+//limit to 6 in the router
+
+/**
+ * limiter, this will limit client side requests so if too many requests come from a single IP it will limit it
+ *  windowMs: time in Ms at which the rate limit will apply
+ * max: the max amount of requests before the limit applies
+**/
+
+const limiter = rateLimit({
+
+    windowMs:10*60*1000,
+    max:20
+})
+
+/**
+ * This will slow down requests after 3 requests to the endpoint have been made, to slow the load on the server
+ *  windowMs: time in Ms at which the rate limit will apply
+ * delayAfter: this will delay the response after # of requests
+ * delayMS:sepcifies how long the delay will be in Ms
+ 
+**/
+const speedLimiter = slowDown({
+    windowMs:10*60*1000,
+    delayAfter:3,
+    delayMs:()=>2000
+})
 // middleware to allow parsing json bodies
 app.use(express.json())
 app.use(cookieParser());
+app.use(limiter)
+app.use(speedLimiter)
 app.use(routes)
 //for cookies
 
