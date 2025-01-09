12f2d05eecd540b6711a75e9b9f00c9cafe1f51e
”EvanO3” <evan.oni@hotmail.com>
2025-01-08 21:28:08 -0500

    added limiting to the stock router

--- src/Routes/stockRouter.js
+++ src/Routes/stockRouter.js
@@ -1,7 +1,21 @@
 const Router = require("express")
 const router = Router();
 const stockController= require("../controller/stockController")
+const rateLimit = require("express-rate-limit");
+ const slowDown =require("express-slow-down")
 //passport.authenticate('jwt',{session:false})
+const limiter = rateLimit({
+
+    windowMs:10*60*1000,
+    max:3
+})
+const slowLimiter = slowDown({
+  windowMs: 10 * 60 * 1000,
+  delayAfter: 1,
+  delayMs: () => 2000,
+});
+router.use(limiter)
+router.use(slowLimiter)
 router.get("/stocks/", stockController.getStockInfo);
 
 module.exports=router;
