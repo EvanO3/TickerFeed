d62f53de9953e6bfcd539f9812d556b209bf7dbe
”EvanO3” <evan.oni@hotmail.com>
2025-01-09 12:02:23 -0500

    removed news information from schema

--- src/models/Stock.js
+++ src/models/Stock.js
@@ -8,16 +8,6 @@ const stockSchema = new mongoose.Schema({
   marketCap: { type: Number },
   price: { type: Number },
   dailyChange: { type: Number, default: 0 },
-  newsArticles: [
-    {
-      title: { type: String, trim: true },
-      description: { type: String, trim: true },
-      author: { type: String, trim: true },
-      url: { type: String, match: /^https?:\/\// },
-      publishedAt: { type: Date, required: true },
-    },
-  ],
-  lastUpdated: { type: Date, default: Date.now },
 });
 
 const Stocks = mongoose.model('Stock', stockSchema);
