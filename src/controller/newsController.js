0f4eda3fc2fffee5b331fec610dc13f1d9498e57
”EvanO3” <evan.oni@hotmail.com>
2025-01-10 23:51:08 -0500

    added get news by stock symbol

--- src/controller/newsController.js
+++ src/controller/newsController.js
@@ -1,19 +1,77 @@
-    const newsResponse = await polygonlimiter.schedule(()=>axios.get(
+const News = require("../models/news")
+const axios = require("axios")
+const Bottleneck = require("bottleneck");
+const redis = require("../utils/redis");
+require("dotenv").config();
+
+
+const polygonlimiter = new Bottleneck({
+  reservoir: 5,
+  reservoirRefreshInterval: 300000,
+  reservoirRefreshAmount: 5,
+  minTime: 60000,
+});
+
+const getStockNew = async(req, res)=>{
+try{
+    const { stockSymbol } = req.query;
+    if (!stockSymbol) {
+      return res.status(400).json({ msg: "No stock symbol found" });
+    }
+
+    const cachedData = await redis.get(stockSymbol)
+    if(cachedData){
+        console.log("Sending cached data")
+        return res.status(200).json({data:cachedData})
+    }
+
+    const newsResponse = await polygonlimiter.schedule(() =>
+      axios.get(
         `https://api.polygon.io/v2/reference/news?ticker=${stockSymbol}&limit=5`,
         {
-            params:{
-                apikey:process.env.polygon_key
-            }
+          params: {
+            apikey: process.env.polygon_key,
+          },
         }
-));
+      )
+    );
+
+    const newsArticles = newsResponse.data.results.map((article)=>({
+        tickerSymbol:stockSymbol,
+        title:article.title,
+        description:article.description,
+        author:article.author,
+        url:article.article_url,
+        publishedAt: new Date(article.published_utc),
+
+    }));
+
+    // await News.insertMany(newsArticles,{ordered:false})
+
+      const news = await News.updateOne(
+        { "newsArticles.tickerSymbol": stockSymbol },
+        {
+          $push: { newsArticles: { $each: newsArticles } },
+          $setOnInsert: { newsArticles: [] },
+        },
+        { upsert: true }
+      );
+  
+     await redis.setEx(stockSymbol, 300, JSON.stringify(newsArticles));
+
+    res.status(200).json({data:newsArticles})
 
 
-// this will be used for the news data
 
-//  newsArticles: newsResponse.data.results.map((article) => ({
-//         title: article.title,
-//         description: article.description,
-//         author: article.author,
-//         url: article.article_url,
-//         publishedAt: new Date(article.published_utc),
-//       })),
+}catch(err){
+    console.log(`Error occured ${err}`)
+    return res.status(500).json({msg:'Internal server error'})
+}
+
+    }
+
+
+
+
+
+module.exports = {getStockNew};
