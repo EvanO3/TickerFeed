bf0fc09368ac30442844c34852e1c07bfff0c299
”EvanO3” <evan.oni@hotmail.com>
2025-01-10 23:50:38 -0500

    added news router

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,11 @@
 const Router = require("express")
 const UserRoutes = require("../Routes/Users.js")
 const StockRoutes = require("../Routes/stockRouter.js")
+const NewsRouter = require("../Routes/newsRouter.js")
 const router =Router()
 
 
 router.use("/api",UserRoutes)
 router.use("/api", StockRoutes)
+router.use("/api", NewsRouter);
 module.exports=router;
