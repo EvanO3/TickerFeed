6870172d89c4a86ae1987c1e5a3f8c1bc42c1684
”EvanO3” <evan.oni@hotmail.com>
2025-01-15 23:26:30 -0500

    added bare bone form with styling

--- front-end/TickerFeedFE/src/components/auth.jsx
+++ front-end/TickerFeedFE/src/components/auth.jsx
@@ -1,60 +1,70 @@
 import React from 'react'
 import "../index.css"
-import * as Form from "@radix-ui/react-form";
-
-function auth() {
+function auth({FormType}) {
   return (
-    <div className="Form">
-      <Form.Root>
-        <Form.Field className="FormField" name="firstname">
-          <Form.Label className="FormLabel">First Name</Form.Label>
-          <Form.Message className="FormMessage" match="valueMissing">
-            Please Enter your First Name
-          </Form.Message>
-          <Form.Control asChild>
-            <input className="Input" type="text" required />
-          </Form.Control>
-        </Form.Field>
-        <Form.Field className="FormField" name="lastname">
-          <Form.Label className="FormLabel">Last Name</Form.Label>
-          <Form.Message className="FormMessage" match="valueMissing">
-            Please Enter your Last Name
-          </Form.Message>
-          <Form.Control asChild>
-            <input className="Input" type="text" required />
-          </Form.Control>
-        </Form.Field>
+    <div className=" bg-slate-400">
+      <h2 className=" font-serif p-2 text-2xl text-white mt-1">TickerSymbol</h2>
+      <div className="h-screen flex items-center justify-center bg-slate-400">
+        <form
+          action=""
+          className="bg-white p-8 rounded-lg shadow-md space-y-6 w-full max-w-sm"
+        >
+          <h2 className="font-serif text-2xl font-bold text-gray-800 text-center">
+            Sign In
+          </h2>
+          <div className="form-control">
+            <label htmlFor="" className=" font-serif block text-gray-600">
+              {" "}
+              First Name
+            </label>
+            <input
+              type="text"
+              placeholder="Please Enter your first name"
+              className="input input-bordered w-full p-2  border border-gray-300 rounded-lg  bg-slate-100"
+            />
+          </div>
 
-        <Form.Field className="FormField" name="email">
-          <Form.Label className="FormLabel">Email</Form.Label>
-          <Form.Message className="FormMessage" match="valueMissing">
-            Please Enter your Email Name
-          </Form.Message>
-          <Form.Message className="FormMessage" match="typeMismatch">
-            Please Enter a valid email
-          </Form.Message>
-          <Form.Control asChild>
-            <input className="Input" type="email" required />
-          </Form.Control>
-        </Form.Field>
+          <div className="form-control">
+            <label htmlFor="" className=" font-serif block text-gray-600">
+              Last Name
+            </label>
+            <input
+              type="text"
+              placeholder="Please Enter your last name"
+              className="input input-bordered w-full p-2  border border-gray-300 rounded-lg  bg-slate-100"
+            />
+          </div>
 
-        <Form.Field className="FormField" name="password">
-          <Form.Label className="FormLabel">Password</Form.Label>
-          <Form.Message className="FormMessage" match="valueMissing">
-            Please Enter a password 
-          </Form.Message>
-          <Form.Control asChild>
-            <input className="Input" type="password" required />
-          </Form.Control>
-        </Form.Field>
+          <div className="form-control">
+            <label htmlFor="" className=" font-serif block text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Please Enter your email"
+              className="input input-bordered w-full p-2  border border-gray-300 rounded-lg  bg-slate-100"
+            />
+          </div>
 
-        <Form.Submit asChild>
-          <button className ="Button"></button>
-        </Form.Submit>
-      </Form.Root>
+          <div className="form-control">
+            <label htmlFor="" className=" font-serif block text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Please Enter a password"
+              className="input input-bordered w-full p-2  border border-gray-300 rounded-lg bg-slate-100"
+            />
+          </div>
 
-
-
+          <button
+            type="submit"
+            className=" font-serif w-full  bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-slate-600"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
