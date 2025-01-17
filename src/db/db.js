71f3e992618550ab6fee8d5d6aca97e805708d18
”EvanO3” <evan.oni@hotmail.com>
2025-01-16 21:52:01 -0500

    removed process.exit

--- src/db/db.js
+++ src/db/db.js
@@ -5,9 +5,8 @@ const connectDB = async()=>{
         await mongoose
           .connect(process.env.MONGODB_URI)
           .then(() => console.log("Connected to DB"))
-          .catch((err) => {console.error(`Error: ${err}`);
-          //process.exit will exit the connect if it fails to connect to the DB
-            process.exit(1);
+          .catch((err) => {console.error(`Error: ${err.message}`);
+          
         });
 
     }catch(error){
