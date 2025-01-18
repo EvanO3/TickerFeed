94c4f753a28da110e5013aa2b8a19873d9db073c
”EvanO3” <evan.oni@hotmail.com>
2025-01-17 22:36:07 -0500

    added routes for the app

--- front-end/TickerFeedFE/src/App.jsx
+++ front-end/TickerFeedFE/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import SignUp from "./pages/SignUp"
 import SignIn from "./pages/SignIn";
+import HomePage from "./pages/homePage"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/Signup" element={<SignUp />} />
         <Route path="/Signin" element={<SignIn />} />
+        <Route path="/home" element={<HomePage/>}/>
       </Routes>
     </BrowserRouter>
   );
